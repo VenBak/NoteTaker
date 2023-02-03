16766e0cf460a1b6c44aab0cb8aa8dcbcb33816a
VenBak <andre.brahin@gmail.com>
2023-02-02 22:40:31 -0500

    added comments to js file

--- paths/api.js
+++ paths/api.js
@@ -1,31 +1,26 @@
-
+// Creates a route within a route using Router()
 const notes = require('express').Router(); 
+// Adds the dependencies 
 const fs = require('fs'); 
 const path = require('path'); 
+// Gets the route to the db file
 const route = path.join(__dirname, '../db/db.json');
 
-function uuid() {
-    // Gets random number between 1 and 1000
-    Math.floor(Math.random * 1000)
-}
-
+// Sends the file as a response when user comes to the homepage
 notes.get('/', (req, res) => {
     res.sendFile(route);
 });
 
-// 
+// Post method to update the html page based on changes to the notes database
 notes.post('/', (req, res) => {
 
     const { title, text } = req.body;
-
-    // If there is a title and text
+    // If there is a title and text run the command
     if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuid()
-        };
-
+        // Creates a new object called new note with parameters
+        const newNote = { title, text }
+    
+    // Reads the db.json file to get the information located in it
     fs.readFile(route, 'utf8', (error, data) => {
         // logs error if the db has no objects
         if (error) {
@@ -37,20 +32,18 @@ notes.post('/', (req, res) => {
             // Push it to an array
             parsedNote.push(newNote);
 
+            // Creates a new db.json file but with the newNote if the user has entered one
             fs.writeFile(route, JSON.stringify(parsedNote),
-            (error) =>
-            error
-                ? console.error(error)
-                : console.info('Success')
+            (error) => error ? console.error(error) : console.info('Success')
             );
         }
     })
-
         const response = {
             status: 'success',
             body: newNote,
         }
 
+        // Responds back to the user with the new note object contained within the response object
         res.json(response);
 
     } else {
